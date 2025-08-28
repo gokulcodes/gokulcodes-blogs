ab61cb028fed5b291c102584c1c104e8f7f3793b
Gokul Varadan <gokulvaradan2202@gmail.com>
2025-08-28 08:13:29 +0530

    rendering simple optimization done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
 
+const socials = [
+  {
+    id: 1,
+    name: "Twitter",
+    url: "https://twitter.com/gokul_varadan",
+    logo: "/twitter.png",
+  },
+  {
+    id: 2,
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/gokulvaradan",
+    logo: "/linkedin.png",
+  },
+  {
+    id: 3,
+    name: "GitHub",
+    url: "https://github.com/gokulcodes",
+    logo: "/github.png",
+  },
+];
+
 export default function Header() {
   return (
     <header className="flex items-center py-4 pt-8 w-11/12 justify-between md:w-full border-b border-black/10 dark:border-white/10 text-white">
@@ -14,46 +35,25 @@ export default function Header() {
           priority
         />
       </Link>
-      <div className="flex gap-4">
-        <a
-          target="_blank"
-          className="opacity-80 hover:opacity-100"
-          href="https://github.com/gokulcodes"
-        >
-          <Image
-            src="/github.png"
-            alt="GitHub logo"
-            className="h-4 w-4 dark:invert"
-            width={24}
-            height={24}
-          />
-        </a>
-        <a
-          target="_blank"
-          className="opacity-80 hover:opacity-100"
-          href="https://twitter.com/gokul_varadan"
-        >
-          <Image
-            src="/twitter.png"
-            alt="Twitter logo"
-            className="h-4 w-4 dark:invert"
-            width={24}
-            height={24}
-          />
-        </a>
-        <a
-          target="_blank"
-          className="opacity-80 hover:opacity-100"
-          href="https://www.linkedin.com/in/gokulvaradan"
-        >
-          <Image
-            src="/linkedin.png"
-            alt="LinkedIn logo"
-            className="h-4 w-4 dark:invert"
-            width={24}
-            height={24}
-          />
-        </a>
+      <div className="flex gap-6">
+        {socials.map((social) => {
+          return (
+            <a
+              key={social.id}
+              target="_blank"
+              className="opacity-60 hover:opacity-100"
+              href={social.url}
+            >
+              <Image
+                src={social.logo}
+                alt={`${social.name} logo`}
+                className="h-5 w-5 dark:invert"
+                width={24}
+                height={24}
+              />
+            </a>
+          );
+        })}
       </div>
     </header>
   );
