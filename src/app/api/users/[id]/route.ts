be8046c1188f52af247344a011984b31966d9e91
Gokul Varadan <gokulvaradan2202@gmail.com>
2025-08-29 09:43:40 +0530

    headless image generation done

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -1,20 +1,19 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import puppeteer from "puppeteer";
 
 // Handle GET requests to /api/users
-export async function GET(
-  request: NextRequest,
-  { params }: { params: Promise<{ id: string }> }
-) {
-  // Fetch data from a database or other source
-  // const users = [
-  //   { id: 1, name: "Alice" },
-  //   { id: 2, name: "Bob" },
-  // ];
-  // const url = request.;
-  // const queryParams = url.split("?")[1];
-  // const params = new URLSearchParams(request.url);
-  // const id = params.get("id");
-  // console.log(params);
-  const awaitedParams = await params;
-  return NextResponse.json({ id: awaitedParams.id });
+export async function GET() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://gokulcodes.dev/");
+  const screenshot = await page.screenshot();
+  await browser.close();
+
+  const imageBuffer = Buffer.from(screenshot);
+  return new NextResponse(imageBuffer, {
+    status: 200,
+    headers: {
+      "Content-Type": "image/jpeg", // Set the correct MIME type
+    },
+  });
 }
